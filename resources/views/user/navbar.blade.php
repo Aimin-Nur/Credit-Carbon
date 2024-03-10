8eb38fb53ee2453b46592efe0ee9b575b2b012ac
muhaimin nur <you@example.com>
2024-03-10 23:01:38 +0800

    fixed menu navbar user : claim credit carbon

--- resources/views/user/navbar.blade.php
+++ resources/views/user/navbar.blade.php
@@ -167,15 +167,27 @@
                   </span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/claimCreditCarbon" >
-                  <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#navbar-extra" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false" >
+                  <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-credit-card-pay" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" /><path d="M3 10h18" /><path d="M16 19h6" /><path d="M19 16l3 3l-3 3" /><path d="M7.005 15h.005" /><path d="M11 15h2" /></svg>
                   </span>
                   <span class="nav-link-title">
-                    Claim Credit Carbon
+                    Credit Carbon
                   </span>
                 </a>
+                <div class="dropdown-menu">
+                  <div class="dropdown-menu-columns">
+                    <div class="dropdown-menu-column">
+                      <a class="dropdown-item" href="/claimCreditCarbon">
+                        Claim Credit Carbon
+                      </a>
+                      <a class="dropdown-item" href="/viewHistory">
+                        History Claim Point
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
